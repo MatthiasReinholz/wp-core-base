6e6929001304069c68f421695a37ebb5083deb40
Matthias Reinholz <9655284+MatthiasReinholz@users.noreply.github.com>
2026-04-18 13:06:43 +0200

    Prepare wp-core-base release v1.3.4

--- .wp-core-base/framework.php
+++ .wp-core-base/framework.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return array (
   'repository' => 'MatthiasReinholz/wp-core-base',
-  'version' => '1.3.3',
+  'version' => '1.3.4',
   'release_channel' => 'stable',
   'distribution' => 
   array (
