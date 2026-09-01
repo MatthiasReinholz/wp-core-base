7fd93d836b09a60dfc732182af7e20a0dc83ef94
MatthiasReinholz <9655284+MatthiasReinholz@users.noreply.github.com>
2026-09-01 12:11:12 +0000

    Prepare wp-core-base release v1.4.8

--- .wp-core-base/framework.php
+++ .wp-core-base/framework.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return array (
   'repository' => 'MatthiasReinholz/wp-core-base',
-  'version' => '1.4.7',
+  'version' => '1.4.8',
   'release_channel' => 'stable',
   'distribution' =>
   array (
